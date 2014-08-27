bdf822c3d9fe7c7668da55550698998d7af72e06
luk32@home <luk32@o2.pl>
2014-08-27 08:06:08 +0200

    Added Finishable interface for objects that need to do execute some callbacks after performing a task.

---
+++ Strategix/src/com/me/mygdxgame/Finishable.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.me.mygdxgame;
+
+/**
+ * A simple interface for objects that need to callback some method after finishing some task.
+ * @author luk32
+ */
+public interface Finishable {
+	public void finish();
+}
